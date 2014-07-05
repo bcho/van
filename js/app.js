f33ca16080bfb060aa5ab2a3fda3e39198eb760f
hbc <bcxxxxxx@gmail.com>
2014-07-06 04:27:46 +0800

    Init with basic theme.

---
+++ js/app.js
@@ -0,0 +1,17 @@
+(function () {
+    Mousetrap.bind(': h e l p', function () {
+        console.log('hello folk!');
+    });
+
+    Mousetrap.bind(': p r o g r a m', function () {
+        console.log('navigate!');
+    });
+
+    Mousetrap.bind('shift+;', function () {
+        console.log('here');
+    });
+
+    Mousetrap.bind('esc', function () {
+        console.log('here');
+    });
+})();
