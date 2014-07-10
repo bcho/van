da0304ec09da49e298cdc725c9906de12e92d34e
hbc <bcxxxxxx@gmail.com>
2014-07-10 20:58:14 +0800

    oops... I'm sleepy :'o

--- js/app.js
+++ js/app.js
@@ -1,17 +1,34 @@
 (function () {
-    Mousetrap.bind(': h e l p', function () {
-        console.log('hello folk!');
+    var $commandPrompt = $('.editor-command-prompt'),
+        commandPromptCommandText = $('.command-prompt-state-command').text(),
+        commandPromptNormalText = $('.command-prompt-state-normal').text(),
+        commandPromptInsertText = $('.command-prompt-state-insert').text();
+
+    Mousetrap.bind(':  h e l p', function () {
+        location.href = 'help.html';
     });
 
     Mousetrap.bind(': p r o g r a m', function () {
-        console.log('navigate!');
+        location.href = 'program.html';
+    });
+
+    Mousetrap.bind(': g i t h u b', function () {
+        location.href = 'https://github.com';
+    });
+
+    Mousetrap.bind(': c o d e r w a l l', function () {
+        location.href = 'https://coderwall.com';
+    });
+
+    Mousetrap.bind(': q', function () {
+        window.open('', '_self').close();
     });
 
     Mousetrap.bind('shift+;', function () {
-        console.log('here');
+        $commandPrompt.text(commandPromptCommandText);
     });
 
     Mousetrap.bind('esc', function () {
-        console.log('here');
+        $commandPrompt.text(commandPromptNormalText);
     });
 })();
